36f6b0e64e0bc7697960e882139931371a2bda14
victor braz <victor.90braz@gmail.com>
2023-10-02 17:00:16 +0200

    add notes to controller

--- routes.php
+++ routes.php
@@ -3,9 +3,9 @@
 return [
   "/" => "./controllers/HomeController.php",
   "/about" => "./controllers/AboutController.php",
-  "/notes" => "./controllers/NotesController.php",
-  "/note" => "./controllers/NoteController.php",
-  "/notes/create" => "./controllers/NotesCreateController.php",
+  "/notes" => "./controllers/notes/NotesController.php",
+  "/note" => "./controllers/notes/NoteController.php",
+  "/notes/create" => "./controllers/notes/NotesCreateController.php",
 ];
 
 ?>
