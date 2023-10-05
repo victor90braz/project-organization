c3bf902cbe4017170682e3ff089d97840a1fc9b6
victor braz <victor.90braz@gmail.com>
2023-10-05 16:50:17 +0200

    refactor create.php

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -1,15 +1,6 @@
 <?php
 
-use Core\Database;
-use Core\Response;
-
-$config = require base_path('config.php');
-$db = new Database($config['database']);
-$currentUserId = Response::CURRENT_USER_ID;
-
-$errors = [];
-
 view("notes/create.view.php", [
     'heading' => 'Create Note',
-    'errors' => $errors
+    'errors' => []
 ]);
