e1bf6aacbb4c782b35ea298fa3159c5dbb9e5c89
victor braz <victor.90braz@gmail.com>
2023-10-05 16:48:32 +0200

    validation issue

--- controllers/notes/store.php
+++ controllers/notes/store.php
@@ -14,14 +14,20 @@
       $errors['body'] = 'A body of no more than 1,000 characters is required.';
   }
 
-  if (empty($errors)) {
-      $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
-          'body' => $_POST['body'],
-          'user_id' => $currentUserId
-      ]);
-
-      header('location: /notes');
-      die();
+  if (! empty($errors)) {
+    return view("notes/create.view.php", [
+      'heading' => 'Create Note',
+      'errors' => $errors
+    ]);
   }
+
+  $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+      'body' => $_POST['body'],
+      'user_id' => $currentUserId
+  ]);
+
+  header('location: /notes');
+  die();
+
 
 ?>
