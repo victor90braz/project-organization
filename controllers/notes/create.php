02baba4ebf12196582492eb826fc55b116828575
victor braz <victor.90braz@gmail.com>
2023-10-05 16:46:06 +0200

    clean up and refactor

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -2,26 +2,12 @@
 
 use Core\Database;
 use Core\Response;
-use Core\Validator;
 
 $config = require base_path('config.php');
 $db = new Database($config['database']);
 $currentUserId = Response::CURRENT_USER_ID;
 
 $errors = [];
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (! Validator::string($_POST['body'], 1, 1000)) {
-        $errors['body'] = 'A body of no more than 1,000 characters is required.';
-    }
-
-    if (empty($errors)) {
-        $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
-            'body' => $_POST['body'],
-            'user_id' => $currentUserId
-        ]);
-    }
-}
 
 view("notes/create.view.php", [
     'heading' => 'Create Note',
---
+++ controllers/notes/store.php
@@ -0,0 +1,27 @@
+<?php
+
+use Core\Validator;
+use Core\Database;
+use Core\Response;
+
+$config = require base_path('config.php');
+$db = new Database($config['database']);
+$currentUserId = Response::CURRENT_USER_ID;
+
+$errors = [];
+
+if (! Validator::string($_POST['body'], 1, 1000)) {
+    $errors['body'] = 'A body of no more than 1,000 characters is required.';
+}
+
+if (empty($errors)) {
+    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => $currentUserId
+    ]);
+
+    header('location: /notes');
+    die();
+}
+
+?>
