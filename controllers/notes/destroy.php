eac3c34422fc91c707ee5d7b3dac5ccde25dab33
victor braz <victor.90braz@gmail.com>
2023-10-05 16:39:08 +0200

    create a destroy page

---
+++ controllers/notes/destroy.php
@@ -0,0 +1,23 @@
+<?php
+
+use Core\Database;
+use Core\Response;
+
+$config = require base_path('config.php');
+$db = new Database($config['database']);
+
+$currentUserId = Response::CURRENT_USER_ID;
+
+
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_POST['id']
+    ])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    $db->query('delete from notes where id = :id', [
+        'id' => $_POST['id']
+    ]);
+
+    header('location: /notes');
+    exit();
--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -8,29 +8,13 @@ $db = new Database($config['database']);
 
 $currentUserId = Response::CURRENT_USER_ID;
 
-// Kinda gross, yes? We'll refactor toward a cleaner approach in episode 33.
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $note = $db->query('select * from notes where id = :id', [
-        'id' => $_GET['id']
-    ])->findOrFail();
+$note = $db->query('select * from notes where id = :id', [
+    'id' => $_GET['id']
+])->findOrFail();
 
-    authorize($note['user_id'] === $currentUserId);
+authorize($note['user_id'] === $currentUserId);
 
-    $db->query('delete from notes where id = :id', [
-        'id' => $_GET['id']
-    ]);
-
-    header('location: /notes');
-    exit();
-} else {
-    $note = $db->query('select * from notes where id = :id', [
-        'id' => $_GET['id']
-    ])->findOrFail();
-
-    authorize($note['user_id'] === $currentUserId);
-
-    view("notes/show.view.php", [
-        'heading' => 'Note',
-        'note' => $note
-    ]);
-}
+view("notes/show.view.php", [
+    'heading' => 'Note',
+    'note' => $note
+]);
