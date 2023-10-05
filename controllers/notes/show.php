9cf5aea6c56e847f75bbe951c225bbb9ecc9d723
victor braz <victor.90braz@gmail.com>
2023-10-05 13:36:43 +0200

    delete post if you are autorized

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -5,16 +5,36 @@ use Core\Response;
 
 $config = require base_path('config.php');
 $db = new Database($config['database']);
-
 $currentUserId = Response::CURRENT_USER_ID;
 
-$note = $db->query('select * from notes where id = :id', [
-    'id' => $_GET['id']
-])->findOrFail();
 
-authorize($note['user_id'] === $currentUserId);
+if($_SERVER["REQUEST_METHOD"] === 'POST') {
+    $currentNoteId = $_GET['id'];
 
-view("notes/show.view.php", [
-    'heading' => 'Note',
-    'note' => $note
-]);
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    $db->query('delete from notes WHERE id = :id', [
+        'id' => $currentNoteId
+    ]);
+
+    header('location: /notes');
+    exit();
+} else {
+
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    view("notes/show.view.php", [
+        'heading' => 'Note',
+        'note' => $note
+    ]);
+}
+
+
